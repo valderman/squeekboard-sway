eaf925e30b9cd1af093b0d149d33673b66cf60f9
Hysterical Raisins <sybolt-dev@xs4all.nl>
2019-07-04 22:11:24 +0200

    Cleanups: fix g_type_init() deprecation warning
    None of this seems necessary anymore. This file could be axed, but leave
    it for now.

--- eek/eek.c
+++ eek/eek.c
@@ -38,9 +38,5 @@
 void
 eek_init (void)
 {
-    g_type_init ();
-
-    g_type_class_ref (EEK_TYPE_SYMBOL);
-    g_type_class_ref (EEK_TYPE_KEYSYM);
-    g_type_class_ref (EEK_TYPE_TEXT);
+    /* void */
 }
