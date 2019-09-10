c725cd7f147b272a20a2e55e5b79b2747285fc21
Dorota Czaplejewicz <dorota.czaplejewicz@puri.sm>
2019-09-10 10:15:25 +0000

    cargo: Allow for manual cargo invokations

--- cargo.sh
+++ cargo.sh
@@ -1,13 +1,22 @@
-#!/bin/sh
+#!/bin/bash
+
+# This script manages Cargo operations
+# while keeping the artifact directory within the build tree
+# instead of the source tree
+
 set -e
+
+SOURCE_DIR="$1"
 
 export CARGO_TARGET_DIR=`pwd`
 if [ ! -z ${2} ]; then
     OUT_PATH=`realpath "${2}"`
 fi
 
-cd $1
-cargo $3 -p rs
+cd $SOURCE_DIR
+shift
+shift
+cargo $BUILD_ARG $@
 
 if [ ! -z ${OUT_PATH} ]; then
     cp "${CARGO_TARGET_DIR}"/debug/librs.a "${OUT_PATH}"
