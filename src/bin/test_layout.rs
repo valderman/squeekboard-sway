cfa66d81827ec4ba4d091fc60ebee62da25f3d3e
Mark Müller <markmueller86@gmail.com>
2019-12-14 10:55:37 +0000

    squeekboard-test-layout: add argument parsing and some more output

--- src/bin/test_layout.rs
+++ src/bin/test_layout.rs
@@ -1,8 +1,16 @@
+#[macro_use]
+extern crate clap;
 extern crate rs;
 
 use rs::tests::check_layout_file;
-use std::env;
 
 fn main() -> () {
-    check_layout_file(env::args().nth(1).expect("No argument given").as_str());
+    let matches = clap_app!(test_layout =>
+        (name: "squeekboard-test-layout")
+        (about: "Test keyboard layout for errors. Returns OK or an error message containing further information.")
+        (@arg INPUT: +required "Yaml keyboard layout file to test")
+    ).get_matches();
+    if check_layout_file(matches.value_of("INPUT").unwrap()) == () {
+        println!("Test result: OK");
+    }
 }
